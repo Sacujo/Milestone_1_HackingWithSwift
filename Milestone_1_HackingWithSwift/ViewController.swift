8d3df3fcccb2ae216dd8697775793b0be4695aa1
Sacujo <isacujo@gmail.com>
2022-10-30 22:47:30 +0300

    initial commit

--- Milestone_1_HackingWithSwift/ViewController.swift
+++ Milestone_1_HackingWithSwift/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var flagsOFCountries = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix("2x.png") {
+                flagsOFCountries.append(item)
+            }
+        }
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flagsOFCountries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        let countryName = String(flagsOFCountries[indexPath.row].dropLast(7)).uppercased()
+        cell.textLabel?.text = countryName
+        let image: UIImage! = UIImage(named: flagsOFCountries[indexPath.row])
+        cell.imageView!.image = image
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Flag") as? FlagViewController {
+            vc.selectedImage = flagsOFCountries[indexPath.row]
+            vc.position = indexPath.row + 1
+            vc.pictureCount = flagsOFCountries.count
+            navigationController?.pushViewController(vc, animated: true)
+        }
+        
+    }
 }
-
